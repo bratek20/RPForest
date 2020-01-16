f98bfa30c705c7ff4a17bebeaf667d5a3864c066
Bartosz Rudzki <bartoszrudzkilo14@gmail.com>
2020-01-16 18:01:06 +0100

    Install script added.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install cmake
+sudo apt install libglm-dev
+sudo apt install libglew-dev
+sudo apt install libglfw3-dev
+sudo apt install libopenexr-dev
+sudo apt install openexr-viewers
+sudo apt install libtbb-dev
+
+./build.sh
