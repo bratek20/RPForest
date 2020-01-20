11eeff704ff9babeae1290c817764876ab5a14e0
Bartosz Rudzki <bartoszrudzkilo14@gmail.com>
2020-01-20 01:27:52 +0100

    Dead configuration. Guide tweaks.

--- src/generating/lsystems/Leaf.cpp
+++ src/generating/lsystems/Leaf.cpp
@@ -4,6 +4,10 @@
 #include "Random.h"
 
 void Leaf::process(ProcessContext& pc)  {
+    if(Assets::LEAF_GENERATORS.size() == 0) {
+        return;
+    }
+    
     ModelPtr leaf = Random::randomElement(Assets::LEAF_GENERATORS)->get()->copy(); 
     leaf->apply(pc.current.getWorldMat());
     pc.model->add(leaf);
