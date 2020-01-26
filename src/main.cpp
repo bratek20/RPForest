09a0dc95bfc6e996b66a5735645acab540641e6f
Bartosz Rudzki <bartoszrudzkilo14@gmail.com>
2020-01-26 17:11:03 +0100

    Main exception handling.

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,77 @@
-#include "Window.h"
-#include "Input.h"
-#include "Globals.h"
-#include "Scene.h"
-#include "Color.h"
 #include "Assets.h"
+#include "Color.h"
+#include "Globals.h"
+#include "Input.h"
+#include "Scene.h"
+#include "Window.h"
 
-#include <iostream>
 #include <algorithm>
 #include <iomanip>
+#include <iostream>
 
 #include "Random.h"
-#include "Turtle.h"
 #include "Shapes.h"
+#include "Turtle.h"
 using namespace std;
 
 ScenePtr scene;
 Turtle t;
 
-void takePhoto(){
-	scene->takePhotoPathTracing();
+void takePhoto() {
+    scene->takePhotoPathTracing();
 }
 
 void debugRay() {
-	scene->debugRay();
+    scene->debugRay();
 }
 
-int main(int argc, char* argv[]){
-	if(argc < 2){
-		cerr << "Config path not provided. Example program execution: ./rpforest.exe ../configs/default" << endl;
-		return -1;
-	}
-	string configFolder = argv[1];    
-	unsigned seed = argc > 2 ? atoi(argv[2]) : Random::SEED_NOT_SET;
-	
-	Random::init(seed);
-	Assets::init(configFolder);
-	Globals::init();
-	
-	if(!Window::open("RPForest")){
-        return -1;
+int main(int argc, char* argv[]) {
+    try {
+        if (argc < 2) {
+            cerr << "Config path not provided. Example program execution: "
+                    "./rpforest.exe ../configs/default"
+                 << endl;
+            return -1;
+        }
+        string configFolder = argv[1];
+        unsigned seed = argc > 2 ? atoi(argv[2]) : Random::SEED_NOT_SET;
+
+        Random::init(seed);
+        Assets::init(configFolder);
+        Globals::init();
+
+        if (!Window::open("RPForest")) {
+            return -1;
+        }
+
+        Assets::loadProgram3D();
+        Materials::init();
+        scene = Scene::create();
+
+        if (Globals::debug) {
+            Input::init();
+            Input::onKeyPressed(GLFW_KEY_P, takePhoto);
+            // Input::onKeyPressed(GLFW_KEY_L, debugRay);
+
+            while (!Window::shouldClose()) {
+                Input::handle();
+
+                scene->update();
+
+                Window::clear();
+                scene->render();
+                Window::swapBuffers();
+
+                Globals::updateTime();
+            }
+            Assets::clear();
+        } else {
+            takePhoto();
+        }
+
+        Window::close();
+    } catch (exception e) {
+        cerr << "Exception cought: " << e.what() << endl;
     }
-
-	Assets::loadProgram3D();
-	Materials::init();	
-	scene = Scene::create();
-
-	if(Globals::debug){		
-		Input::init();
-		Input::onKeyPressed(GLFW_KEY_P, takePhoto);
-		//Input::onKeyPressed(GLFW_KEY_L, debugRay);
-
-		while(!Window::shouldClose()){
-			Input::handle();
-
-			scene->update();
-
-			Window::clear();
-			scene->render();
-			Window::swapBuffers();
-
-			Globals::updateTime();
-		}	
-		Assets::clear();
-	}
-	else{
-		takePhoto();
-	}	
-	
-	Window::close();
-	return 0;
+    return 0;
 }
-
-
